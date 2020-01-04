fe10c0f96b15a7dc6d103bbef33054eae1021a44
Tomas <t.plastiak@gmail.com>
2020-01-04 22:56:56 +0100

    json picovina

--- kalendar/meniny.js
+++ kalendar/meniny.js
@@ -1,36 +1,24 @@
-function loadXMLDoc() {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-            myFunction(this);
-        }
-    };
-    xmlhttp.open("GET", "xml_meniny.xml", true);
-    xmlhttp.send();
-}
-function myFunction(xml) {
-    var i;
-    var xmlDoc = xml.responseXML;
-    var table="<tr><th>Den</th><th>index</th></tr>";
-    var x = xmlDoc.getElementsByTagName("zaznam");
-    for (i = 0; i <x.length; i++) {
-        table += "<tr><td>" +
-            x[i].getElementsByTagName("den")[0].childNodes[0].nodeValue +
-            "</td><td>" +i+
-        "</td></tr>" ;
-    }
-    document.getElementById("demo").innerHTML = table;
-}
+// function loadXMLDoc() {
+//     var xmlhttp = new XMLHttpRequest();
+//     xmlhttp.onreadystatechange = function() {
+//         if (this.readyState === 4 && this.status === 200) {
+//             myFunction(this);
+//         }
+//     };
+//     xmlhttp.open("GET", "xml_meniny.xml", true);
+//     xmlhttp.send();
+// }
 
+//
 // function myFunction(xml) {
 //     var day, month, year,datum;
-//     // $('#id').on('change', function() {
+//     // $('#id').on('click', function() {
 //     var date = $('#date1').val().split("-");
 //     day = date[2];
 //     month = date[1];
 //     year = date[0];
 //     datum=month+day;
-//     alert(datum);
+//      // console.log(datum);
 //     // });
 //
 //     var x, i, xmlDoc, txt;
@@ -41,9 +29,51 @@ function myFunction(xml) {
 //     // alert(x[i].childNodes[0].nodeValue);
 //     for (i = 0; i< x.length; i++) {
 //         if(x[i].childNodes[0].nodeValue===datum){
-//             var pom = xmlDoc.getElementsByTagName("HU");
-//             txt += pom[i].childNodes[0].nodeValue + "<br>";
+//             if(datum > 0229){
+//                 // --i;
+//                 var pom = xmlDoc.getElementsByTagName("HU");
+//                 txt += pom[i-1].childNodes[0].nodeValue;
+//             }
+//             else {
+//                 var pom = xmlDoc.getElementsByTagName("HU");
+//                 txt += pom[i].childNodes[0].nodeValue;
+//             }
 //         }
 //     }
 //     document.getElementById("demo").innerHTML = txt;
 // }
+
+function loadXMLDoc() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myObj = JSON.parse(this.responseText);
+            myFunctionDatumNaMeno(myObj);
+        }
+    };
+    xmlhttp.open("GET", "meniny.json", true);
+    xmlhttp.send();
+}
+
+function myFunctionDatumNaMeno(json) {
+    var txt,day, month, year,datum;
+    var date = $('#date1').val().split("-");
+    day = date[2];
+    month = date[1];
+    year = date[0];
+    datum=month+day;
+    txt="";
+    for (var key in json) {
+        if (json.hasOwnProperty(key)) {
+            if(json[key].den===datum){
+                txt+=json[key].SK;
+            }
+        }
+    }
+    document.getElementById("demo").innerHTML = txt;
+}
+
+function myFunctionMenoNaDatum() {
+
+}
+
