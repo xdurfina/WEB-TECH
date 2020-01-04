3da62f46b498de546ade743055110e97dabff5ee
xdurfina <durfina.jaro@gmail.com>
2020-01-04 13:06:25 +0100

    pridanie svg obrazkov

--- animacie/script.js
+++ animacie/script.js
@@ -9,49 +9,59 @@
 //     });
 // };
 
-var path = anime.path("#cesta1");
-var path2 = anime.path("#cesta2");
-var path3 = anime.path("#cesta3");
+const path = anime.path("#cesta1");
+const path2 = anime.path("#cesta2");
+const path3 = anime.path("#cesta3");
 
+const poradieaut = [];
 
-var easings = ['linear', 'easeInCubic', 'easeOutCubic', 'easeInOutCubic'];
-
-
-var motionPath1 = anime({
-    targets: '.box.red',
-    translateX: path('x'),
-    translateY: path('y'),
-    rotate: path('angle'),
-    easing: function (el, i) {
-        return easings[i];
-    },
-    duration: 10000,
-    loop: true
-});
+// const easings = ['linear', 'easeInCubic', 'easeOutCubic', 'easeInOutCubic'];
 
 
 
-var motionPath2 = anime({
+
+function motionPath1() {
+    modreauto.style.marginTop = "0px";
+    anime({
+        targets: '.box.red',
+        translateX: path('x'),
+        translateY: path('y'),
+        rotate: path('angle'),
+        easing: 'easeInOutCubic',
+        duration: 5000,
+        loop: false
+    });
+    const spravneporadie = [1,2,3];
+    poradieaut.push(1);
+    poradieaut.push(2);
+    poradieaut.push(3);
+    if (poradieaut.sort() === spravneporadie) {
+        console.log("sikokot");
+    } else {console.log(poradieaut.sort())}
+}
+
+
+const modreauto = document.getElementById("modreauto");
+modreauto.addEventListener('click', motionPath1);
+
+
+const motionPath2 = anime({
     targets: '.box.blue',
     translateX: path2('x'),
     translateY: path2('y'),
     rotate: path2('angle'),
-    easing: function (el, i) {
-        return easings[i];
-    },
-    duration: 10000,
+    easing: 'easeInOutCubic',
+    duration: 5000,
     loop: true
 });
 
-var motionPath3 = anime({
+const motionPath3 = anime({
     targets: '.box.yellow',
     translateX: path3('x'),
     translateY: path3('y'),
     rotate: path3('angle'),
-    easing: function (el, i) {
-        return easings[i];
-    },
-    duration: 10000,
+    easing: 'easeInOutCubic',
+    duration: 5000,
     loop: true
 });
 
