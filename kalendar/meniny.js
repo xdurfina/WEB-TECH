21ebb713453dd0b63b8627681e35fa7e9b6f8637
Tomas <t.plastiak@gmail.com>
2020-01-05 23:07:28 +0100

    upravit este vypis

--- kalendar/meniny.js
+++ kalendar/meniny.js
@@ -46,7 +46,7 @@
 function loadXMLDoc1() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
             var myObj = JSON.parse(this.responseText);
             myFunctionDatumNaMeno(myObj);
         }
@@ -57,7 +57,7 @@ function loadXMLDoc1() {
 function loadXMLDoc2() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
             var myObj = JSON.parse(this.responseText);
             myFunctionMenoNaDatum(myObj);
         }
@@ -68,7 +68,7 @@ function loadXMLDoc2() {
 function loadXMLDoc3() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
             var myObj = JSON.parse(this.responseText);
             myFunctionMeninyAktualnyDen(myObj);
         }
@@ -82,7 +82,7 @@ function myFunctionDatumNaMeno(json) {
     var date = $('#date1').val().split("-");
     day = date[2];
     month = date[1];
-    year = date[0];
+    // year = date[0];
     datum=month+day;
     txt="";
     for (var key in json) {
@@ -139,41 +139,21 @@ function myFunctionMenoNaDatum(json) {
         if (json.hasOwnProperty(key)){
             if(json[key].SK){
                 var pom;
-                // var pomocne="Tomas";
-                // var kokot=pomocne.split(',')
-                //     alert(kokot[0]);
-                //     alert(kokot[1]);
-                //     alert(kokot.length);
-                // var meniny=pomocne.split(', ');
+
                 pom=json[key].SK;
                 var mena=pom.split(', ');
-                if(mena[1]!==undefined){
-                    alert("asdasdasa");
-                    var data_array = pom.split(', ');
-                    console.log(data_array);
-                    for(var toto in data_array){
-                        toto=toto.toLocaleLowerCase();
-                        toto=toto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-                        // alert(toto);
-                        if(toto===res){
-                            txt+=json[key].den;
-                            break;
-                        }
-                    }
-                }
-                else
-                {   pom=mena[0];
-                    pom = pom.toLocaleLowerCase();
-                    pom = pom.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-                    if (pom === res) {
-                        txt += json[key].den;
+
+                for(var i=0;i<mena.length;i++){
+                    var temp_meno;
+                    temp_meno=mena[i].toLocaleLowerCase();
+                    temp_meno=temp_meno.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+                    if(temp_meno===res){
+                        txt+=json[key].den;
                         break;
                     }
                 }
             }
-
         }
     }
-
     document.getElementById("demo2").innerHTML = txt;
 }
