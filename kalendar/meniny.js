86ee810b90adc20cc1a37a1d8c6e73accdc15022
Tomas <t.plastiak@gmail.com>
2020-01-07 22:10:23 +0100

    finalka

--- kalendar/meniny.js
+++ kalendar/meniny.js
@@ -1,48 +1,3 @@
-// function loadXMLDoc() {
-//     var xmlhttp = new XMLHttpRequest();
-//     xmlhttp.onreadystatechange = function() {
-//         if (this.readyState === 4 && this.status === 200) {
-//             myFunction(this);
-//         }
-//     };
-//     xmlhttp.open("GET", "xml_meniny.xml", true);
-//     xmlhttp.send();
-// }
-
-//
-// function myFunction(xml) {
-//     var day, month, year,datum;
-//     // $('#id').on('click', function() {
-//     var date = $('#date1').val().split("-");
-//     day = date[2];
-//     month = date[1];
-//     year = date[0];
-//     datum=month+day;
-//      // console.log(datum);
-//     // });
-//
-//     var x, i, xmlDoc, txt;
-//     xmlDoc = xml.responseXML;
-//     txt = "";
-//     // alert("poadsas");
-//     x = xmlDoc.getElementsByTagName("den");
-//     // alert(x[i].childNodes[0].nodeValue);
-//     for (i = 0; i< x.length; i++) {
-//         if(x[i].childNodes[0].nodeValue===datum){
-//             if(datum > 0229){
-//                 // --i;
-//                 var pom = xmlDoc.getElementsByTagName("HU");
-//                 txt += pom[i-1].childNodes[0].nodeValue;
-//             }
-//             else {
-//                 var pom = xmlDoc.getElementsByTagName("HU");
-//                 txt += pom[i].childNodes[0].nodeValue;
-//             }
-//         }
-//     }
-//     document.getElementById("demo").innerHTML = txt;
-// }
-
 function loadXMLDoc1() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
@@ -94,7 +49,6 @@ function myFunctionDatumNaMeno(json) {
                     if(json[key].SKsviatky){
                         txt+=json[key].SKsviatky;
                     }
-                    // break;
                 }
             }
         }
